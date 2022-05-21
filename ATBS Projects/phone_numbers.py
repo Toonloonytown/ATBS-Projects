fe4920246e4944c3a11ab78ed430d95b7c1776c4
Jon Smith <jonksmith1990@csu.fullerton.edu>
2022-05-21 03:55:12 -0700

    Changed indexing finshed non regex version

--- ATBS Projects/phone_numbers.py
+++ ATBS Projects/phone_numbers.py
@@ -9,7 +9,7 @@ def is_phone_number(number):
         elif (i < 7):
             if not number[i].isdecimal():
                 return False
-        elif (i == 8):
+        elif (i == 7):
             if number[i] != '-': return False
         else:
             if (not number[i].isdecimal()): 
